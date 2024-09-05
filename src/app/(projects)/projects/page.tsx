07af63f9bb4c463cf70e61d89a04e20bf3330a48
Colby Fayock <fayock@gmail.com>
2024-09-04 23:35:11 -0400

    Step 4: Adding a list of links to the dynamic pages from MDX files

--- src/app/(projects)/projects/page.tsx
+++ src/app/(projects)/projects/page.tsx
@@ -1,3 +1,8 @@
+import { promises as fs } from 'fs'
+import path from 'path';
+import Link from 'next/link';
+import { compileMDX } from 'next-mdx-remote/rsc';
+
 import Section from '@/components/Section';
 import Container from '@/components/Container';
 import Article from '@/components/Article';
@@ -8,6 +13,27 @@ export const metadata = {
 }
 
 export default async function Projects() {
+  const filenames = await fs.readdir(path.join(process.cwd(), 'src/projects'));
+
+  interface Frontmatter {
+    title: string;
+  }
+
+  const projects = await Promise.all(filenames.map(async (filename) => {
+    const content = await fs.readFile(path.join(process.cwd(), 'src/projects', filename), 'utf-8');
+    const { frontmatter } = await compileMDX<Frontmatter>({
+      source: content,
+      options: {
+        parseFrontmatter: true
+      }
+    })
+    return {
+      filename,
+      slug: filename.replace('.mdx', ''),
+      ...frontmatter
+    }
+  }))
+
   return (
     <Section spacing="compact">
       <Container>
@@ -17,6 +43,15 @@ export default async function Projects() {
 
         <Article withSidebar={false}>
           <h2 className="sr-only">Project Ideas</h2>
+          <ul>
+            {projects.map(({ title, slug }) => {
+              return (
+                <li>
+                  <Link href={`/projects/${slug}`}>{ title }</Link>
+                </li>
+              )
+            })}
+          </ul>
         </Article>
       </Container>
     </Section>
